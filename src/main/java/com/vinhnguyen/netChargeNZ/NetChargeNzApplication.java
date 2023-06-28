2a3ad1acdb7eadaeebdba7ab1d3e58411c7124b3
Vinh Nguyen <vinh.nglx@gmail.com>
2023-06-28 14:46:35 +1200

    App: Init the Spring App
    With dependencies:
    - Web
    - JPA
    - H2
    - Lombok

---
+++ src/main/java/com/vinhnguyen/netChargeNZ/NetChargeNzApplication.java
@@ -0,0 +1,13 @@
+package com.vinhnguyen.netChargeNZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetChargeNzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetChargeNzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinhnguyen/netChargeNZ/NetChargeNzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinhnguyen.netChargeNZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetChargeNzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
